bb03f5182bc8ce64dc781df3d6a357552eba9346
Dan Harrin <git@danharrin.com>
2021-12-16 17:36:43 +0000

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,16 +25,19 @@ class DatabaseSeeder extends Seeder
             'name' => 'Demo User',
             'email' => 'admin@filamentadmin.com',
         ]);
+        $this->command->info('Admin user created.');
 
         // Blog
         $blogCategories = BlogCategory::factory()->count(20)->create();
+        $this->command->info('Blog categories created.');
         Author::factory()->count(20)
             ->has(
                 Post::factory()->count(10)
-                    ->state(fn (array $attributes, Author $author) => ['blog_category_id' => $blogCategories->pluck('id')->random()]),
+                    ->state(fn (array $attributes, Author $author) => ['blog_category_id' => $blogCategories->random(1)->first()->id]),
                 'posts'
             )
             ->create();
+        $this->command->info('Blog authors and posts created.');
 
         // Shop
         $categories = ShopCategory::factory()->count(20)
@@ -42,25 +45,31 @@ class DatabaseSeeder extends Seeder
                 ShopCategory::factory()->count(3),
                 'children'
             )->create();
+        $this->command->info('Shop categories created.');
         $brands = Brand::factory()->count(20)->create();
+        $this->command->info('Shop brands created.');
         $customers = Customer::factory()->count(1000)->create();
+        $this->command->info('Shop customers created.');
         Discount::factory()->count(20)->create();
-        $products = Product::factory()->count(200)
-            ->sequence(fn ($sequence) => ['shop_brand_id' => $brands->pluck('id')->random()])
+        $this->command->info('Shop discounts created.');
+        $products = Product::factory()->count(50)
+            ->sequence(fn ($sequence) => ['shop_brand_id' => $brands->random(1)->first()->id])
             ->hasAttached($categories->random(rand(3, 6)), ['created_at' => now(), 'updated_at' => now()])
             ->has(
                 Review::factory()->count(rand(10, 20))
-                    ->state(fn (array $attributes, Product $product) => ['shop_customer_id' => $customers->pluck('id')->random()]),
+                    ->state(fn (array $attributes, Product $product) => ['shop_customer_id' => $customers->random(1)->first()->id]),
                 'reviews'
             )
             ->create();
+        $this->command->info('Shop products created.');
         Order::factory()->count(1000)
-            ->sequence(fn ($sequence) => ['shop_customer_id' => $customers->pluck('id')->random()])
+            ->sequence(fn ($sequence) => ['shop_customer_id' => $customers->random(1)->first()->id])
             ->has(
                 OrderItem::factory()->count(rand(2, 5))
-                    ->state(fn (array $attributes, Order $order) => ['shop_product_id' => $products->pluck('id')->random()]),
+                    ->state(fn (array $attributes, Order $order) => ['shop_product_id' => $products->random(1)->first()->id]),
                 'items'
             )
             ->create();
+        $this->command->info('Shop orders created.');
     }
 }
