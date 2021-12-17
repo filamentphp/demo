e34170bf4efd27f181264ee7b5e3600d30812e31
Shuvro Roy <shuvro.nsu.cse@gmail.com>
2021-12-17 12:25:50 +0600

    wip

--- app/Filament/Widgets/CustomersChart.php
+++ app/Filament/Widgets/CustomersChart.php
@@ -16,7 +16,7 @@ class CustomersChart extends LineChartWidget
             'datasets' => [
                 [
                     'label' => 'Customers',
-                    'data' => [4344, 5676, 6798, 7890, 8987, 9388, 10343, 10524, 13664, 14345, 15753, ],
+                    'data' => [4344, 5676, 6798, 7890, 8987, 9388, 10343, 10524, 13664, 14345, 15753],
                 ],
             ],
             'labels' => ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
