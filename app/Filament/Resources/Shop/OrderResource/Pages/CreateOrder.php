51789b27a279a3b83e1e5add6eee45ce29264219
Dan Harrin <git@danharrin.com>
2022-08-09 13:53:29 +0100

    Update CreateOrder.php

--- app/Filament/Resources/Shop/OrderResource/Pages/CreateOrder.php
+++ app/Filament/Resources/Shop/OrderResource/Pages/CreateOrder.php
@@ -8,16 +8,4 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateOrder extends CreateRecord
 {
     protected static string $resource = OrderResource::class;
-
-    protected function mutateFormDataBeforeCreate(array $data): array
-    {
-        $total = 0;
-        foreach ($data['items'] as $item) {
-            $total += $item['unit_price'] * $item['qty'];
-        }
-
-        $data['total_price'] = $total;
-
-        return $data;
-    }
 }
