8df248fe4569ae11e9f57683173b7690704746e9
Shuvro Roy <shuvro.nsu.cse@gmail.com>
2022-09-10 22:18:21 +0600

    chore: after order create database notification added

--- app/Filament/Resources/Shop/OrderResource/Pages/CreateOrder.php
+++ app/Filament/Resources/Shop/OrderResource/Pages/CreateOrder.php
@@ -3,9 +3,26 @@
 namespace App\Filament\Resources\Shop\OrderResource\Pages;
 
 use App\Filament\Resources\Shop\OrderResource;
+use Filament\Notifications\Actions\Action;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateOrder extends CreateRecord
 {
     protected static string $resource = OrderResource::class;
+
+    protected function afterCreate(): void
+    {
+        $order = $this->record;
+
+        Notification::make()
+            ->title('New order')
+            ->icon('heroicon-o-shopping-bag')
+            ->body("**{$order->customer->name} ordered {$order->items->count()} products.**")
+            ->actions([
+                Action::make('View')
+                    ->url(OrderResource::getUrl('edit', ['record' => $order])),
+            ])
+            ->sendToDatabase(auth()->user());
+    }
 }
