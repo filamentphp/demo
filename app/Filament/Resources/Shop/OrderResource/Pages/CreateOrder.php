775f4923133d19f56a8a8ef50a68a23a5b294055
Ziyaan Hassan <ziyaan2010@gmail.com>
2022-09-30 09:49:40 +0500

    Feat: CreateOrder Wizard

--- app/Filament/Resources/Shop/OrderResource/Pages/CreateOrder.php
+++ app/Filament/Resources/Shop/OrderResource/Pages/CreateOrder.php
@@ -3,12 +3,17 @@
 namespace App\Filament\Resources\Shop\OrderResource\Pages;
 
 use App\Filament\Resources\Shop\OrderResource;
+use Filament\Forms\Components\Card;
+use Filament\Forms\Components\Wizard\Step;
 use Filament\Notifications\Actions\Action;
 use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
+use Filament\Resources\Pages\CreateRecord\Concerns\HasWizard;
 
 class CreateOrder extends CreateRecord
 {
+    use HasWizard;
+
     protected static string $resource = OrderResource::class;
 
     protected function afterCreate(): void
@@ -24,5 +29,20 @@ class CreateOrder extends CreateRecord
                     ->url(OrderResource::getUrl('edit', ['record' => $order])),
             ])
             ->sendToDatabase(auth()->user());
+    }
+
+    protected function getSteps(): array
+    {
+        return [
+            Step::make('Order Details')
+                ->schema([
+                    Card::make(OrderResource::getFormSchema())->columns(),
+                ]),
+
+            Step::make('Order Items')
+                ->schema([
+                    Card::make(OrderResource::getFormSchema('items')),
+                ]),
+        ];
     }
 }
