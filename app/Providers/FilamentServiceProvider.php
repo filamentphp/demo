321ff3100035406076e5fd3a6e3e4703d7bccd65
Zep Fietje <hey@zepfietje.com>
2022-10-01 14:44:03 +0200

    Fix typo

--- app/Providers/FilamentServiceProvider.php
+++ app/Providers/FilamentServiceProvider.php
@@ -5,13 +5,8 @@ namespace App\Providers;
 use Filament\Facades\Filament;
 use Illuminate\Support\ServiceProvider;
 
-class FilamentSeviceProvider extends ServiceProvider
+class FilamentServiceProvider extends ServiceProvider
 {
-    public function register(): void
-    {
-        
-    }
-
     public function boot(): void
     {
         Filament::registerNavigationGroups([
