eb9403b3ad9150bbf8e299d8aef9fcace3abcd53
ZedoX <75579178+Z3d0X@users.noreply.github.com>
2022-10-01 09:52:17 +0500

    Feat: CreateOrder Wizard (#50)

--- app/Filament/Resources/Shop/OrderResource.php
+++ app/Filament/Resources/Shop/OrderResource.php
@@ -41,98 +41,11 @@ class OrderResource extends Resource
                 Forms\Components\Group::make()
                     ->schema([
                         Forms\Components\Card::make()
-                            ->schema([
-                                Forms\Components\TextInput::make('number')
-                                    ->default('OR-' . random_int(100000, 999999))
-                                    ->disabled()
-                                    ->required(),
-
-                                Forms\Components\Select::make('shop_customer_id')
-                                    ->relationship('customer', 'name')
-                                    ->searchable()
-                                    ->required()
-                                    ->createOptionForm([
-                                        Forms\Components\TextInput::make('name')
-                                            ->required(),
-
-                                        Forms\Components\TextInput::make('email')
-                                            ->required()
-                                            ->email()
-                                            ->unique(),
-
-                                        Forms\Components\TextInput::make('phone'),
-                                    ])
-                                    ->createOptionAction(function (Forms\Components\Actions\Action $action) {
-                                        return $action
-                                            ->modalHeading('Create customer')
-                                            ->modalButton('Create customer')
-                                            ->modalWidth('lg');
-                                    }),
-
-                                Forms\Components\Select::make('status')
-                                    ->options([
-                                        'new' => 'New',
-                                        'processing' => 'Processing',
-                                        'shipped' => 'Shipped',
-                                        'delivered' => 'Delivered',
-                                        'cancelled' => 'Cancelled',
-                                    ])
-                                    ->required(),
-
-                                Forms\Components\Select::make('currency')
-                                    ->searchable()
-                                    ->getSearchResultsUsing(fn (string $query) => Currency::where('name', 'like', "%{$query}%")->pluck('name', 'id'))
-                                    ->getOptionLabelUsing(fn ($value): ?string => Currency::find($value)?->getAttribute('name'))
-                                    ->required(),
-
-                                AddressForm::make('address')
-                                    ->columnSpan('full'),
-
-                                Forms\Components\MarkdownEditor::make('notes')
-                                    ->columnSpan('full'),
-                            ])
+                            ->schema(static::getFormSchema())
                             ->columns(2),
 
                         Forms\Components\Section::make('Order items')
-                            ->schema([
-                                Forms\Components\Repeater::make('items')
-                                    ->relationship()
-                                    ->schema([
-                                        Forms\Components\Select::make('shop_product_id')
-                                            ->label('Product')
-                                            ->options(Product::query()->pluck('name', 'id'))
-                                            ->required()
-                                            ->reactive()
-                                            ->afterStateUpdated(fn ($state, callable $set) => $set('unit_price', Product::find($state)?->price ?? 0))
-                                            ->columnSpan([
-                                                'md' => 5,
-                                            ]),
-
-                                        Forms\Components\TextInput::make('qty')
-                                            ->numeric()
-                                            ->default(1)
-                                            ->columnSpan([
-                                                'md' => 2,
-                                            ])
-                                            ->required(),
-
-                                        Forms\Components\TextInput::make('unit_price')
-                                            ->label('Unit Price')
-                                            ->disabled()
-                                            ->numeric()
-                                            ->required()
-                                            ->columnSpan([
-                                                'md' => 3,
-                                            ]),
-                                    ])
-                                    ->orderable()
-                                    ->defaultItems(1)
-                                    ->disableLabel()
-                                    ->columns([
-                                        'md' => 10,
-                                    ])
-                                    ->required(),
-                            ]),
+                            ->schema(static::getFormSchema('items')),
                     ])
                     ->columnSpan(['lg' => fn (?Order $record) => $record === null ? 3 : 2]),
 
@@ -284,5 +197,101 @@ class OrderResource extends Resource
     protected static function getNavigationBadge(): ?string
     {
         return static::$model::where('status', 'new')->count();
+    }
+
+    public static function getFormSchema(?string $section = null): array
+    {
+        if ($section === 'items') {
+            return [
+                Forms\Components\Repeater::make('items')
+                    ->relationship()
+                    ->schema([
+                        Forms\Components\Select::make('shop_product_id')
+                            ->label('Product')
+                            ->options(Product::query()->pluck('name', 'id'))
+                            ->required()
+                            ->reactive()
+                            ->afterStateUpdated(fn ($state, callable $set) => $set('unit_price', Product::find($state)?->price ?? 0))
+                            ->columnSpan([
+                                'md' => 5,
+                            ]),
+
+                        Forms\Components\TextInput::make('qty')
+                            ->numeric()
+                            ->default(1)
+                            ->columnSpan([
+                                'md' => 2,
+                            ])
+                            ->required(),
+
+                        Forms\Components\TextInput::make('unit_price')
+                            ->label('Unit Price')
+                            ->disabled()
+                            ->numeric()
+                            ->required()
+                            ->columnSpan([
+                                'md' => 3,
+                            ]),
+                    ])
+                    ->orderable()
+                    ->defaultItems(1)
+                    ->disableLabel()
+                    ->columns([
+                        'md' => 10,
+                    ])
+                    ->required(),
+            ];
+        }
+
+        return [
+            Forms\Components\TextInput::make('number')
+                ->default('OR-' . random_int(100000, 999999))
+                ->disabled()
+                ->required(),
+
+            Forms\Components\Select::make('shop_customer_id')
+                ->relationship('customer', 'name')
+                ->searchable()
+                ->required()
+                ->createOptionForm([
+                    Forms\Components\TextInput::make('name')
+                        ->required(),
+
+                    Forms\Components\TextInput::make('email')
+                        ->required()
+                        ->email()
+                        ->unique(),
+
+                    Forms\Components\TextInput::make('phone'),
+                ])
+                ->createOptionAction(function (Forms\Components\Actions\Action $action) {
+                    return $action
+                        ->modalHeading('Create customer')
+                        ->modalButton('Create customer')
+                        ->modalWidth('lg');
+                }),
+
+            Forms\Components\Select::make('status')
+                ->options([
+                    'new' => 'New',
+                    'processing' => 'Processing',
+                    'shipped' => 'Shipped',
+                    'delivered' => 'Delivered',
+                    'cancelled' => 'Cancelled',
+                ])
+                ->required(),
+
+            Forms\Components\Select::make('currency')
+                ->searchable()
+                ->getSearchResultsUsing(fn (string $query) => Currency::where('name', 'like', "%{$query}%")->pluck('name', 'id'))
+                ->getOptionLabelUsing(fn ($value): ?string => Currency::find($value)?->getAttribute('name'))
+                ->required(),
+
+            AddressForm::make('address')
+                ->columnSpan('full'),
+
+            Forms\Components\MarkdownEditor::make('notes')
+                ->columnSpan('full'),
+        ];
     }
 }
