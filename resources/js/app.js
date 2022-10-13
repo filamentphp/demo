c37531d15fbb6300aada2115c20617de6fb7296b
Dan Harrin <git@danharrin.com>
2022-10-13 09:18:40 +0100

    wip

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
-import './bootstrap';
+import Alpine from 'alpinejs'
+import FormsAlpinePlugin from '../../vendor/filament/forms/dist/module.esm'
+
+Alpine.plugin(FormsAlpinePlugin)
+
+window.Alpine = Alpine
+
+Alpine.start()
--- routes/web.php
+++ routes/web.php
@@ -1,1 +1,5 @@
 <?php
+
+use App\Http\Livewire\Form;
+
+\Illuminate\Support\Facades\Route::get('form', Form::class);
