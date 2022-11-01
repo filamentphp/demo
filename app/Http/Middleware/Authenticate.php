e07b1d57255a9a023ccf6cddc199f80cb0c6f8bf
Dan Harrin <git@danharrin.com>
2022-11-01 13:57:45 +0000

    Update Authenticate.php

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use Filament\Facades\Filament;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
@@ -14,8 +15,6 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
-        if (! $request->expectsJson()) {
-            return route('login');
-        }
+        return Filament::getLoginUrl();
     }
 }
