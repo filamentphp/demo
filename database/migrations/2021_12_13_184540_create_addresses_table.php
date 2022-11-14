894a5f55f0fb8015f38fa31257ccaface793bffc
Zep Fietje <hey@zepfietje.com>
2022-11-14 21:08:29 +0100

    Fix dropping wrong table in migration down method

--- database/migrations/2021_12_13_184540_create_addresses_table.php
+++ database/migrations/2021_12_13_184540_create_addresses_table.php
@@ -32,6 +32,6 @@ return new class() extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('addresses');
+        Schema::dropIfExists('shop_order_addresses');
     }
 };
