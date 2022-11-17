a4bce492d0e14feba8d9012237eb7877c9bfa948
Dan Harrin <git@danharrin.com>
2022-11-17 10:28:31 +0000

    wip

--- app/Filament/Resources/Shop/OrderResource.php
+++ app/Filament/Resources/Shop/OrderResource.php
@@ -53,11 +53,11 @@ class OrderResource extends Resource
                     ->schema([
                         Forms\Components\Placeholder::make('created_at')
                             ->label('Created at')
-                            ->content(fn (Order $record): string => $record->created_at?->diffForHumans()),
+                            ->content(fn (Order $record): ?string => $record->created_at?->diffForHumans()),
 
                         Forms\Components\Placeholder::make('updated_at')
                             ->label('Last modified at')
-                            ->content(fn (Order $record): string => $record->updated_at?->diffForHumans()),
+                            ->content(fn (Order $record): ?string => $record->updated_at?->diffForHumans()),
                     ])
                     ->columnSpan(['lg' => 1])
                     ->hidden(fn (?Order $record) => $record === null),
