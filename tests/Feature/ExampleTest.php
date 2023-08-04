a395fd441b110ca22510bb4c2da5d37c84415272
Shuvro Roy <shuvro.nsu.cse@gmail.com>
2023-08-05 00:02:03 +0600

    chore: update phpunit.xml file

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -13,7 +13,7 @@ class ExampleTest extends TestCase
      */
     public function test_example()
     {
-        $response = $this->get('/');
+        $response = $this->get('/login');
 
         $response->assertStatus(200);
     }
