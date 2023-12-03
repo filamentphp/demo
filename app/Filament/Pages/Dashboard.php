e173cc9d4cb82c07be9e1727bc7d95d883623e3c
Dan Harrin <git@danharrin.com>
2023-12-03 23:46:04 +0000

    Update

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -4,6 +4,7 @@ namespace App\Filament\Pages;
 
 use Filament\Forms\Components\Checkbox;
 use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Form;
 use Filament\Forms\Get;
@@ -18,13 +19,17 @@ class Dashboard extends BaseDashboard
     {
         return $form
             ->schema([
-                Select::make('businessCustomersOnly')
-                    ->boolean(),
-                DatePicker::make('startDate')
-                    ->maxDate(fn (Get $get) => $get('endDate') ?: now()),
-                DatePicker::make('endDate')
-                    ->minDate(fn (Get $get) => $get('startDate') ?: now())
-                    ->maxDate(now()),
+                Section::make()
+                    ->schema([
+                        Select::make('businessCustomersOnly')
+                            ->boolean(),
+                        DatePicker::make('startDate')
+                            ->maxDate(fn (Get $get) => $get('endDate') ?: now()),
+                        DatePicker::make('endDate')
+                            ->minDate(fn (Get $get) => $get('startDate') ?: now())
+                            ->maxDate(now()),
+                    ])
+                    ->columns(3),
             ]);
     }
 }
