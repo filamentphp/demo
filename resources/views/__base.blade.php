c892a77e3b580d7b84c6882bb0887a5a478d445a
Soufian <selhabib@ze-camping.fr>
2024-10-04 13:49:57 +0100

    add js

--- resources/views/__base.blade.php
+++ resources/views/__base.blade.php
@@ -62,7 +62,17 @@
 
         @include('__footer')
 
+        <!-- Plugins JS File -->
+        <script src="{{asset('vendor/template/jquery/jquery.min.js')}}"></script>
+        <script src="{{asset('vendor/template/parallax/parallax.min.js')}}"></script>
+        <script src="{{asset('vendor/template/elevatezoom/jquery.elevatezoom.min.js')}}"></script>
+        <script src="{{asset('vendor/template/magnific-popup/jquery.magnific-popup.min.js')}}"></script>
 
+        <script src="{{asset('vendor/template/owl-carousel/owl.carousel.min.js')}}"></script>
+        <script src="{{asset('vendor/template/imagesloaded/imagesloaded.pkgd.min.js')}}"></script>
+        <script src="{{asset('vendor/template/isotope/isotope.pkgd.min.js')}}"></script>
+        <!-- Main JS File -->
+        <script src="{{asset('js/front/main.min.js')}}"></script>
     </body>
 
 </html>
