a555f5a30c9073d215df0bd0e4615860208fa15c
Soufian <selhabib@ze-camping.fr>
2024-10-11 07:19:22 +0100

    image size

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -14,8 +14,8 @@
 
                                     @foreach($product->getMedia('product-images') as $media)
                                         @if ($media->getUrl())
-                                            <figure class="product-image">
-                                                <img src="{{ $media->getUrl() }}" alt="{{ $product->name }}" style="width: 800px;height: 900px;object-fit: cover;">
+                                            <figure class="product-image" style="width: 800px; height: 900px; overflow: hidden;">
+                                                <img src="{{ $media->getUrl() }}" alt="{{ $product->name }}" style="width: 100%; height: 100%; object-fit: contain;">
                                             </figure>
                                         @endif
                                     @endforeach
