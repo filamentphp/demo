bec4170d72e76c80fc7b7a156e20951abd7c329a
Soufian <selhabib@ze-camping.fr>
2024-10-11 07:33:58 +0100

    image size

--- resources/views/pages/thank_you.blade.php
+++ resources/views/pages/thank_you.blade.php
@@ -6,8 +6,8 @@
         <main class="main order">
             <div class="page-content pt-7 pb-10 mb-10">
                 <div class="step-by pr-4 pl-4">
-                    <h3 class="title title-simple title-step"><a href="cart.html">1. Shopping Cart</a></h3>
-                    <h3 class="title title-simple title-step"><a href="checkout.html">2. Checkout</a></h3>
+                    <h3 class="title title-simple title-step"><a href="{{route('cart.show')}}">1. Shopping Cart</a></h3>
+                    <h3 class="title title-simple title-step"><a href="{{route('checkout')}}">2. Checkout</a></h3>
                     <h3 class="title title-simple title-step active"><a href="{{route('thankyou')}}">3. Order Complete</a></h3>
                 </div>
                 <div class="container mt-8">
