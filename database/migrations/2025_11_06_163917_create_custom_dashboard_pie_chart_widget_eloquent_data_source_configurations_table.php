d82dcd5ce962a6320aad27536338db94d493619a
Dan Harrin <git@danharrin.com>
2025-11-06 17:05:36 +0000

    new chart types

---
+++ database/migrations/2025_11_06_163917_create_custom_dashboard_pie_chart_widget_eloquent_data_source_configurations_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('custom_dashboard_pie_chart_widget_eloquent_data_source_configurations', function (Blueprint $table) {
+            $table->id();
+            $table->string('operator');
+            $table->string('dimension_attribute');
+            $table->string('measure_attribute')->nullable();
+            $table->boolean('is_cumulative')->default(false);
+            $table->jsonb('filters')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('custom_dashboard_pie_chart_widget_eloquent_data_source_configurations');
+    }
+};
